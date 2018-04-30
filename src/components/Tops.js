be06f1d01cd34f19db652b33265d125fd189ef31
ungear <redeemer@inbox.ru>
2018-04-30 16:39:21 +0300

    top page draft

--- src/components/Tops.js
+++ src/components/Tops.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const Tops = () => (
-  <section>
-    Tops
-  </section> 
-)
+export class Tops extends Component {
+  constructor(){
+    super();
+    this.state = {
+      loading: false,
+      topByLikes: [],
+    }
+  }
+
+  componentDidMount(){
+    this.setState(Object.assign({}, this.state, {loading: true}));
+    setTimeout(_ => {
+      this.setState({
+        loading: false,
+        topByLikes: [
+          {id: 1, name: "Aaaaaa"},
+          {id: 2, name: "Bbbbbbbb"},
+        ]
+      })
+    }, 2000)
+  }
+
+  render(){
+    return (
+      <section>
+        <div>Loading: {this.state.loading.toString()}</div>
+        <div className="top">
+        {this.state.topByLikes.map(x => <div>{x.name}</div>)}
+
+        </div>
+      </section> 
+    )
+  }
+}
