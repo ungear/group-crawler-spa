2d5e8f99cd5769518add00bf031ea5453eb3167d
ungear <redeemer@inbox.ru>
2018-05-06 08:54:46 +0300

    add styles

--- src/components/Tops.js
+++ src/components/Tops.js
@@ -27,12 +27,31 @@ class Tops extends Component {
     return (
       <section>
         <div>Loading: {this.props.isFetching.toString()}</div>
-        <div className="top">
-          {this.props.topByLikes.map(x => <div key={x._id}><div>{x.likes}</div><div>{x.text}</div></div>)}
+        <div className="top" style={style_grid}>
+          {this.props.topByLikes.map(x => 
+            <div key={x._id} style={style_row}>
+              <div style={style_id}>{x.likes}</div>
+              <div style={style_text}>{x.text}</div>
+            </div>
+          )}
         </div>
       </section> 
     )
   }
 }
-
+const style_grid = {
+  margin: "10px",
+  border: "1px solid #ccc"
+}
+const style_row = {
+  display: "flex",
+  padding: "5px",
+  borderBottom: "1px solid #ccc"
+}
+const style_id = {
+  flex: "0 0 50px"  
+}
+const style_text = {
+  flex: "1 1 100%"
+}
 export default connect(mapStateToProps, mapDispatchToProps)(Tops)
